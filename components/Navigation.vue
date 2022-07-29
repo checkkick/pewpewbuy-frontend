ceb029c8899f9cee3d9e235efe1dc4d4bf952b81
checkkick <tuskancik226@gmail.com>
2022-07-29 13:55:30 +0300

    complete navigation wablayout

---
+++ components/Navigation.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="nav">
+    <nav class="nav__filter">
+      <ul class="nav__filter__list">
+        <li
+          v-for="(filterItem, idx) in Object.keys(filterMenu)"
+          :key="idx"
+          class="nav__filter__list__item"
+          :class="{ active: filterItem === chooseFilter }"
+          @click="chooseFilter = filterItem"
+        >
+          {{ filterItem }}
+          <ul
+            v-if="filterItem === chooseFilter"
+            class="nav__filter__dropdown-list"
+          >
+            <li
+              v-for="(item, index) in filterMenu[filterItem]"
+              :key="index"
+              class="nav__filter__dropdown-list__item"
+              :class="{ active: item === chooseSubfilter }"
+              @click="chooseSubfilter = item"
+            >
+              {{ item }}
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <button class="nav__btn-create">Разместить объявление</button>
+  </div>
+</template>
+
+<script setup>
+const chooseFilter = ref('')
+const chooseSubfilter = ref('')
+const filterMenu = {
+  'Страйкбольное оружие': [
+    'АК-серия',
+    'Винтовки',
+    'Пулеметы',
+    'Дробовики',
+    'Автоматы',
+    'Автоматы прочие модели',
+    'АС ВАЛ, ВСС, СР-3М',
+    'Гранатометы',
+  ],
+  'Внешний тюнинг': [
+    'АК-серия',
+    'Винтовки',
+    'Пулеметы',
+    'Дробовики',
+    'Автоматы',
+    'Автоматы прочие модели',
+    'АС ВАЛ, ВСС, СР-3М',
+    'Гранатометы',
+  ],
+  'Внутренний тюнинг': [
+    'АК-серия',
+    'Винтовки',
+    'Пулеметы',
+    'Дробовики',
+    'Автоматы',
+    'Автоматы прочие модели',
+    'АС ВАЛ, ВСС, СР-3М',
+    'Гранатометы',
+  ],
+  Снаряжение: [
+    'АК-серия',
+    'Винтовки',
+    'Пулеметы',
+    'Дробовики',
+    'Автоматы',
+    'Автоматы прочие модели',
+    'АС ВАЛ, ВСС, СР-3М',
+    'Гранатометы',
+  ],
+}
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 42px;
+
+  &__filter {
+    background: $filter-background;
+    border: 1px solid $filter-border;
+    box-shadow: 0px 13px 140px rgba(255, 255, 255, 0.72);
+    border-radius: 91px;
+    padding: 15px 13px;
+
+    &__list {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+      display: flex;
+      align-items: center;
+      justify-content: space-evenly;
+      gap: 15px;
+
+      &__item {
+        @include defineFontMontserrat(500, 24px, 29px);
+        position: relative;
+        text-align: center;
+        padding: 22px 27px;
+        border-radius: 91px;
+        color: $black;
+        &.active {
+          background: $primary;
+          color: $white;
+        }
+      }
+    }
+
+    &__dropdown-list {
+      position: absolute;
+      top: 93px;
+      left: 20px;
+      right: 20px;
+      padding: 0;
+      margin: 0;
+      list-style: none;
+      background: $filter-background;
+      border: 1px solid $filter-border;
+      box-shadow: 0px 13px 12px rgba(0, 0, 0, 0.15);
+      border-radius: 14px;
+
+      &__item {
+        @include defineFontMontserrat(500, 18px, 22px);
+        text-align: center;
+        padding: 14px 17px;
+        color: $black;
+        &.active {
+          background: $primary;
+          color: $white;
+        }
+        &:first-child {
+          border-radius: 14px 14px 0 0;
+        }
+        &:last-child {
+          border-radius: 0 0 14px 14px;
+        }
+      }
+    }
+  }
+
+  &__btn-create {
+    @include defineFontMontserrat(600, 24px, 1);
+    padding: 32px 28px;
+    background: $accent;
+    border: none;
+    border-radius: 91px;
+  }
+}
+</style>
