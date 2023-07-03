f0ed40ec53a0458cc0bea282ff7b617cb56f7fd0
checkkick <tuskancik226@gmail.com>
2023-07-03 15:03:23 +0300

    main page complete

--- components/mobile/profile/Guest.vue
+++ components/mobile/profile/Guest.vue
@@ -1,19 +1,82 @@
 <template>
   <div class="container">
-    <!-- Личный просмотр профиля -->
+    <!-- Гостевой просмотр профиля -->
+
+    <GuestMain
+      v-if="page === 'main'"
+      :user="user"
+      :active-products-length="activeProducts.length"
+      :inactive-products-length="inactiveProducts.length"
+      @change-page="(pageName) => page = pageName"
+    />
+
+    <Reviews
+      v-if="page === 'reviews'"
+      :reviews="user.reviews"
+      @change-page="(pageName) => page = pageName"
+    />
+
+    <DetailPublications
+      v-if="page === 'activePublications'"
+      :products="activeProducts"
+      :my-publications="false"
+      :guest-show="true"
+      @change-page="page = 'main'"
+    >
+      <template #header>
+        <div class="publications-name substrate-mobile">
+          <span class="publications-name__logo publications-name__logo--history" />
+          <p class="publications-name__text">
+            Актив
+          </p>
+        </div>
+      </template>
+    </DetailPublications>
+
+    <DetailPublications
+      v-if="page === 'inactivePublications'"
+      :products="inactiveProducts"
+      :my-publications="false"
+      :guest-show="true"
+      @change-page="page = 'main'"
+    >
+      <template #header>
+        <div class="publications-name substrate-mobile">
+          <span class="publications-name__logo publications-name__logo--history" />
+          <p class="publications-name__text">
+            Неактив
+          </p>
+        </div>
+      </template>
+    </DetailPublications>
   </div>
 </template>
 
-<script>
+<script setup>
+import { clients } from '@/store/clients';
+import GuestMain from './GuestMain.vue';
+import Reviews from './Reviews.vue';
+import DetailPublications from './DetailPublications.vue';
 
-export default {
-  setup() {
-  },
-  data() {
-    return {
-    };
-  },
-};
+const route = useRoute();
+
+const page = ref('main');
+
+const user = ref({});
+const activeProducts = ref([]);
+const inactiveProducts = ref([]);
+
+onMounted(async () => {
+  user.value = await clients().GET_DETAIL_USER(route.params.id);
+
+  user.value.products.forEach((product) => {
+    if (product.status === 'Active') {
+      activeProducts.value.push(product);
+    } else {
+      inactiveProducts.value.push(product);
+    }
+  });
+});
 </script>
 
 <style lang="scss" scoped></style>
---
+++ components/mobile/profile/GuestMain.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="profile">
+    <h3 class="profile__username">
+      {{ user.first_name ? user.first_name : "Имя пользователя" }}
+      {{ user.last_name }}
+    </h3>
+    <div class="profile__about substrate-mobile">
+      <div class="profile__row">
+        <img
+          class="profile__img"
+          :src="user.avatar ? user.avatar : noImage"
+          alt="profile picture"
+        >
+      </div>
+      <div class="profile__row">
+        <p class="profile__title">
+          Рейтинг
+        </p>
+        <RatingCalc :stars="user.rep ? Math.floor(user.rep) : 0" />
+        <p class="profile__title">
+          Игровой позывной
+        </p>
+        <p
+          v-if="user.call_sign"
+          class="profile__text"
+        >
+          {{ user.call_sign }}
+        </p>
+        <p
+          v-else
+          class="profile__text profile__text--inactive"
+        >
+          не заполнено
+        </p>
+        <p class="profile__title">
+          Местоположение
+        </p>
+        <p
+          v-if="user.city"
+          class="profile__text"
+        >
+          {{ user.city }}
+        </p>
+        <p
+          v-else
+          class="profile__text profile__text--inactive"
+        >
+          не заполнено
+        </p>
+      </div>
+    </div>
+    <a
+      href="#"
+      class="profile__link-btn substrate-mobile"
+      @click.prevent="$emit('changePage', 'activePublications')"
+    >{{ activeProductsLength }} активных объявлений</a>
+    <a
+      href="#"
+      class="profile__link-btn substrate-mobile"
+      @click.prevent="$emit('changePage', 'inactivePublications')"
+    >{{ inactiveProductsLength }} проданных товаров</a>
+    <a
+      href="#"
+      class="profile__link-btn substrate-mobile"
+      @click.prevent="showContacts = true"
+    >Социальные сети</a>
+    <a
+      href="#"
+      class="profile__link-btn substrate-mobile"
+      @click.prevent="$emit('changePage', 'reviews')"
+    >Отзывы о продавце</a>
+    <button
+      class="profile__btn"
+      @click="showContacts = true"
+    >
+      Написать продавцу
+    </button>
+
+    <ContactsModal
+      v-if="showContacts"
+      :email="user.email"
+      :vk="user.vk"
+      :tg="user.tg"
+      @close-contacts-window="showContacts = false"
+    />
+  </div>
+</template>
+
+<script setup>
+import RatingCalc from '@/components/profile/RatingCalc.vue';
+import ContactsModal from '@/components/product/ContactsModal.vue';
+import noPhoto from '@/assets/img/no-photo.png';
+
+defineEmits(['changePage']);
+
+defineProps({
+  user: {
+    type: Object,
+    default: () => { },
+  },
+  activeProductsLength: {
+    type: Number,
+    default: 0,
+  },
+  inactiveProductsLength: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const showContacts = ref(false);
+
+const noImage = ref(noPhoto);
+</script>
+
+<style lang="scss" scoped>
+.profile {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  gap: 8px;
+
+  &__username {
+    @include defineFontMontserrat(600, 15px, 1.4);
+    color: $black;
+    margin-bottom: 7px;
+  }
+
+  &__about {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  &__row {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    flex-wrap: wrap;
+    align-items: stretch;
+    gap: 1rem 0.5rem;
+
+    &:last-child {
+      width: 100%;
+    }
+  }
+
+  &__img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+
+  &__line {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  &__title {
+    @include defineFontMontserrat(600, 12px, 15px);
+    text-align: right;
+    letter-spacing: -0.01em;
+  }
+
+  &__text {
+    @include defineFontMontserrat(400, 12px, 15px);
+
+    &.profile__text--inactive {
+      color: $black-inactive;
+    }
+  }
+
+  &__link-btn {
+    @include defineFontMontserrat(600, 13px, 16px);
+    text-decoration: none;
+    color: $black;
+  }
+
+  &__btn {
+    @include defineBtnPrimary(12px, 68px, 15px, 25px);
+    margin-top: 10px;
+    align-self: center;
+  }
+}
+</style>
