01c9f55ceb2b94061cef512e25a8499ca41fbfc2
checkkick <tuskancik226@gmail.com>
2023-08-28 19:59:28 +0300

    default api port is null

--- composables/api.js
+++ composables/api.js
@@ -1,34 +1,38 @@
 import { notifications } from '@/store/notifications';
 
 const config = useRuntimeConfig();
+const apiPort = config.public.API_PORT ? `:${config.public.API_PORT}` : '';
 
-export const api = (url, otherOptions = {}) => $fetch(`${config.public.API_URL}:${config.public.API_PORT}/api/${url}`, {
-  retry: 0,
-  ...otherOptions,
+export const api = (url, otherOptions = {}) => $fetch(
+  `${config.public.API_URL}${apiPort}/api/${url}`,
+  {
+    retry: 0,
+    ...otherOptions,
 
-  // async onResponse({ request, response, options }) {
-  //   console.log('[fetch response]')
-  // },
-  // async onResponseError({ request, response, options }) {
-  //   console.log('[fetch response error]')
-  // },
+    // async onResponse({ request, response, options }) {
+    //   console.log('[fetch response]')
+    // },
+    // async onResponseError({ request, response, options }) {
+    //   console.log('[fetch response error]')
+    // },
 
-  // async onRequest({ request, options }) {
-  //   console.log('[fetch request]')
-  // },
-  // async onRequestError({ request, options, error }) {
-  //   console.log('[fetch request error]')
-  // },
+    // async onRequest({ request, options }) {
+    //   console.log('[fetch request]')
+    // },
+    // async onRequestError({ request, options, error }) {
+    //   console.log('[fetch request error]')
+    // },
 
-  onResponseError({ options }) {
-    const storeNotifications = notifications();
+    onResponseError({ options }) {
+      const storeNotifications = notifications();
 
-    storeNotifications.ADD_NOTIFICATION(
-      options.errorAlert,
-      'Системная ошибка',
-      'alert',
-    );
+      storeNotifications.ADD_NOTIFICATION(
+        options.errorAlert,
+        'Системная ошибка',
+        'alert',
+      );
 
-    return false;
+      return false;
+    },
   },
-});
+);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       API_URL: process.env.API_URL || 'http://localhost',
-      API_PORT: process.env.API_PORT || '8500',
+      API_PORT: process.env.API_PORT || '',
     },
   },
 
