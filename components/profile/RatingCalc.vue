d2b08e5e78e6ccd1a48f02390cf18b0b5ae08196
checkkick <tuskancik226@gmail.com>
2023-09-19 15:23:28 +0300

    change rating to simple

--- components/profile/RatingCalc.vue
+++ components/profile/RatingCalc.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="rating">
     <svg
-      v-for="item in stars"
+      v-for="item in 5"
       :key="item"
+      :class="{ 'star-active': item <= stars }"
       width="18"
       height="18"
       viewBox="0 0 18 18"
@@ -11,20 +12,6 @@
     >
       <path
         d="M8.75 0.75L10.7706 6.96885H17.3095L12.0194 10.8123L14.0401 17.0312L8.75 13.1877L3.45993 17.0312L5.48056 10.8123L0.190492 6.96885H6.72937L8.75 0.75Z"
-        fill="#F8B94E"
-      />
-    </svg>
-    <svg
-      v-for="item in 5 - stars"
-      :key="item"
-      width="19"
-      height="18"
-      viewBox="0 0 19 18"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M9.5 0.75L11.5206 6.96885H18.0595L12.7694 10.8123L14.7901 17.0312L9.5 13.1877L4.20993 17.0312L6.23056 10.8123L0.940492 6.96885H7.47937L9.5 0.75Z"
         fill="#D9D9D9"
       />
     </svg>
@@ -63,5 +50,9 @@ export default {
       height: 15px;
     }
   }
+}
+
+.star-active path {
+  fill: #F8B94E;
 }
 </style>
