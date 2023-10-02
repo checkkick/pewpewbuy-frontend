d6a82fb6a0702af3d97c4fd542919c29265bc26a
checkkick <tuskancik226@gmail.com>
2023-10-02 15:07:10 +0300

    add get self request to mount profile

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -8,12 +8,15 @@
 <script setup>
 import MyDesktop from '@/components/desktop/profile/My.vue';
 import { media } from '@/store/media';
+import { clients } from '@/store/clients';
 import MyMobile from '@/components/mobile/profile/My.vue';
 
 definePageMeta({
   middleware: ['authorization'],
   layout: 'auth-no-nav-layout',
 });
+
+onMounted(() => clients().GET_SELF());
 
 const mobile = computed(() => media().MEDIA_MOBILE);
 </script>
